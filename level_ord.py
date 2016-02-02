06c244f2444a447e9df567a1b9e558e898f32129
franciscovargas <vargfran@gmail.com>
2016-02-02 19:36:14 +0000

    indentation bugs

--- level_ord.py
+++ level_ord.py
@@ -3,27 +3,30 @@ from itertools import chain
 def level_gen(ad_list, root, sh, h):
     """
     creats a dictionary of levels for
-    a tree
+    a tree from an adjecency list representation
+    of a graph
     """
     if sh == 0:
-	return {0: [root]}
+        return {0: [root]}
     else:
         level = h - sh
-	cache = ad_list[root]
+        cache = ad_list[root]
         ad_list[root] = list(chain(*[ad_list[x] for x in ad_list[root] ]))
-	a = level_gen(ad_list,root, sh-1,h)
-	a.update({level+1: cache})
-	return a
+        a = level_gen(ad_list,root, sh-1,h)
+        a[level+1]  = cache
+    return a
 
 
 def find_depth(ad_list, root):
     """
     Find the height of the tree
+    from an adjecency list representation of
+    a graph
     """
     if len(ad_list[root]) < 1:
-	return 0
+        return 0
     else:
-	return max([find_depth(ad_list, x) + 1 for x in ad_list[root] ])
+        return max([find_depth(ad_list, x) + 1 for x in ad_list[root] ])
 
 
 if __name__ == "__main__":
