482fe5a6f59f2d3f31427da6a602baca25e8b41e
franciskv <vargfran@gmail.com>
2017-02-20 22:14:15 +0000

    tabs2space

--- BFS.java
+++ BFS.java
@@ -7,35 +7,35 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class BFS {
-	
-	public static void search(HashMap<String, List<String>> G,  String root){
-		Queue<String> queue = new LinkedList<String>();
-		queue.add(root);
-		
-		HashSet<String> visited = new HashSet<String>();
-		visited.add(root);
-		
-		while(!queue.isEmpty()){
-			String parent = queue.poll();
-			System.out.println(parent);
-			for(String child: G.get(parent)){
-				if(!visited.contains(child)){
-					queue.add(child);
-					visited.add(child);
-				}
-			}
-		}
-	}
-	
-	
-	public static void main(String[] args){
-		HashMap<String, List<String>> G = new HashMap<String, List<String>>();
+    
+    public static void search(HashMap<String, List<String>> G,  String root){
+        Queue<String> queue = new LinkedList<String>();
+        queue.add(root);
+        
+        HashSet<String> visited = new HashSet<String>();
+        visited.add(root);
+        
+        while(!queue.isEmpty()){
+            String parent = queue.poll();
+            System.out.println(parent);
+            for(String child: G.get(parent)){
+                if(!visited.contains(child)){
+                    queue.add(child);
+                    visited.add(child);
+                }
+            }
+        }
+    }
+    
+    
+    public static void main(String[] args){
+        HashMap<String, List<String>> G = new HashMap<String, List<String>>();
 
-		G.put("A", Arrays.asList( "B", "C", "D") );
-		G.put("B", Arrays.asList("C"));
-		G.put("D", Arrays.asList("E"));
-		G.put("E", Collections.<String>emptyList());
-		G.put("C", Collections.<String>emptyList());
-		search(G, "A");
-	}
+        G.put("A", Arrays.asList( "B", "C", "D") );
+        G.put("B", Arrays.asList("C"));
+        G.put("D", Arrays.asList("E"));
+        G.put("E", Collections.<String>emptyList());
+        G.put("C", Collections.<String>emptyList());
+        search(G, "A");
+    }
 }
--- clock_palindrome.py
+++ clock_palindrome.py
@@ -3,44 +3,44 @@ Efficient time palindrome generation
 small fun puzzle.
 """
 def gen_hours():
-	# 70 iterations as opposed to the ~ 1400
-	# brute force solution
-	palindromes = list()
-	for i in range(24):
-		if i < 10:
-			for j in range(6):
-				palindromes.append("{0}:{1}{2}".format(i,j,i))
+    # 70 iterations as opposed to the ~ 1400
+    # brute force solution
+    palindromes = list()
+    for i in range(24):
+        if i < 10:
+            for j in range(6):
+                palindromes.append("{0}:{1}{2}".format(i,j,i))
 
-		else:
-			char_array = str(i)
-			if int(char_array[1]) < 6:
-				palindromes.append("{0}{1}:{1}{0}".format(char_array[0],
-					                                      char_array[1]))
+        else:
+            char_array = str(i)
+            if int(char_array[1]) < 6:
+                palindromes.append("{0}{1}:{1}{0}".format(char_array[0],
+                                                          char_array[1]))
 
-	return palindromes
+    return palindromes
 
 
 
 def palindrome(x):
-	x = x.replace(":","")
-	return x == x[::-1]
+    x = x.replace(":","")
+    return x == x[::-1]
 
 
 def brute_force():
-	palindromes = list()
-	for i in range(24):
-		for j in range(60):
-			if j < 10:
-				j = "0" + str(j)
-			hour = str(i) + ":" + str(j)
-			if palindrome(hour):
-				palindromes.append(hour)
-	return palindromes
+    palindromes = list()
+    for i in range(24):
+        for j in range(60):
+            if j < 10:
+                j = "0" + str(j)
+            hour = str(i) + ":" + str(j)
+            if palindrome(hour):
+                palindromes.append(hour)
+    return palindromes
 
 if __name__ == "__main__":
-	p1 = gen_hours()
-	p2 = brute_force()
-	print all(map(palindrome, p1))
-	print  len(p1), len(p2)
-	print  p1
-	print set(p2) -(set(p1))
+    p1 = gen_hours()
+    p2 = brute_force()
+    print all(map(palindrome, p1))
+    print  len(p1), len(p2)
+    print  p1
+    print set(p2) -(set(p1))
